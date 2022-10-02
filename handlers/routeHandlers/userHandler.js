ede9298a1022b246fa785258db2e41446681beef
sohag292 <sohagshah571@gmail.com>
2022-10-02 13:02:21 +0600

    first commit

---
+++ handlers/routeHandlers/userHandler.js
@@ -0,0 +1,94 @@
+/*
+ * Title: User Handler
+ * Description: Handler to handle user related routes
+ * Author: Sumit Saha ( Learn with Sumit )
+ * Date: 11/21/2020
+ *
+ */
+// dependencies
+const data = require('../../lib/data');
+const { hash } = require('../../helpers/utilities');
+// const { parseJSON } = require('../../helpers/utilities');
+
+// module scaffolding
+const handler = {};
+
+handler.userHandler = (requestProperties, callback) => {
+    const acceptedMethods = ['get', 'post', 'put', 'delete'];
+    if (acceptedMethods.indexOf(requestProperties.method) > -1) {
+        handler._users[requestProperties.method](requestProperties, callback);
+    } else {
+        callback(405);
+    }
+};
+
+handler._users = {};
+
+handler._users.post = (requestProperties, callback) => {
+    const firstName =
+        typeof requestProperties.body.firstName === 'string' &&
+        requestProperties.body.firstName.trim().length > 0
+            ? requestProperties.body.firstName
+            : false;
+
+    const lastName =
+        typeof requestProperties.body.lastName === 'string' &&
+        requestProperties.body.lastName.trim().length > 0
+            ? requestProperties.body.lastName
+            : false;
+
+    const phone =
+        typeof requestProperties.body.phone === 'string' &&
+        requestProperties.body.phone.trim().length === 11
+            ? requestProperties.body.phone
+            : false;
+
+    const password =
+        typeof requestProperties.body.password === 'string' &&
+        requestProperties.body.password.trim().length > 0
+            ? requestProperties.body.password
+            : false;
+
+    const tosAgreement =
+        typeof requestProperties.body.tosAgreement === 'boolean' &&
+        requestProperties.body.tosAgreement
+            ? requestProperties.body.tosAgreement
+            : false;
+
+    if (firstName && lastName && phone && password && tosAgreement) {
+        // make sure that the user doesn't already exists
+        data.read('users', phone, (err1,user) => {
+            if (err1) {
+                const userObject = {
+                    firstName,
+                    lastName,
+                    phone,
+                    password: hash(password),
+                    tosAgreement,
+                };
+                // store the user to db
+                data.create('users', phone, userObject, (err2) => {
+                    if (!err2) {
+                        callback(200, {
+                            message: 'User was created successfully!',
+                        });
+                    } else {
+                        callback(500, { error: 'Could not create user!' });
+                    }
+                });
+            } else {
+                callback(500, {
+                    error: 'There was a problem in server side!',
+                });
+            }
+        });
+    } else {
+        callback(400, {
+            error: 'You have a problem in your request',
+        });
+    }
+};
+
+
+
+module.exports = handler;
